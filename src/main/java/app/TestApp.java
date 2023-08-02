f97d6f18aa5ea9d043acfca44584f1794af7a1eb
LAPTOP-EGI1T3KO\T14 Gen 3 <encorec42923@gmail.com>
2023-08-02 18:45:03 +0800

    main Test

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -1,8 +1,63 @@
 package app;
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import core.util.HibernateUtil;
+import web.member.pojo.Member;
+
 public class TestApp {
 	public static void main(String[] args) {
 		
-	}
+		TestApp app = new TestApp();
+//		Member member = new Member();
+//		member.setUsername("root");
+//		member.setPassword("12345678");
+//		member.setNickname("lee");
+//		
+//		
+//		app.insert(member);
+//		System.out.println(member.getId());
+		
+		System.out.println(app.deleteById(3));
+		
+	};
+	
+	public Integer insert(Member member) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			session.persist(member);
+			transaction.commit();
+			return member.getId();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return null;
+		} 
+		
+	};
+	
+	public Integer deleteById(Integer id) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Member member = session.get(Member.class, id);
+			session.remove(member);
+			transaction.commit();
+			return 1;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return -1;
+		} 
+		
+	};
 
-}
+};
