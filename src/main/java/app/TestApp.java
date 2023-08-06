1639874719a8d134a3455ef00c6f0a9f890c7863
LAPTOP-EGI1T3KO\T14 Gen 3 <encorec42923@gmail.com>
2023-08-06 09:08:03 +0800

    8/5

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -1,9 +1,17 @@
 package app;
 
 
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import core.util.HibernateUtil;
 import web.member.pojo.Member;
@@ -21,8 +29,51 @@ public class TestApp {
 //		app.insert(member);
 //		System.out.println(member.getId());
 		
-		System.out.println(app.deleteById(3));
 		
+		//test case
+		
+//		member.setId(1);
+//		member.setPass(true);
+//		
+//		System.out.println(app.updateById(member));
+		
+//		System.out.println(app.selectById(2).getNickname());
+		
+		
+		
+//		app.selectAll().forEach(member -> System.out.println(member.getNickname()));
+		
+		//or
+		
+//		for(Member member : app.selectAll()) {
+//			System.out.println(member.getNickname());
+//		}
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		
+
+
+
+		CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+		CriteriaQuery<Member> criteriaQuery = criteriaBuilder.createQuery(Member.class);
+
+		//	from MEMBER		
+		Root<Member> root = criteriaQuery.from(Member.class);
+		
+		//	where USERNAME = ? and PASSWORD = ?		
+		criteriaQuery.where(criteriaBuilder.and(
+				criteriaBuilder.equal(root.get("username"), "admin"),
+				criteriaBuilder.equal(root.get("password"), "P@ssW0rd")
+				));
+		
+		//		select USERNAME, NICKNAME
+		criteriaQuery.multiselect(root.get("username"),root.get("nickname"));
+		
+		// order by ID
+		criteriaQuery.orderBy(criteriaBuilder.asc(root.get("id")));
+		
+		Member member = session.createQuery(criteriaQuery).uniqueResult();
+		System.out.println(member.getNickname());
 	};
 	
 	public Integer insert(Member member) {
@@ -56,6 +107,71 @@ public class TestApp {
 			e.printStackTrace();
 			session.getTransaction().rollback();
 			return -1;
+		} 
+		
+	};
+	
+	
+	public Integer updateById(Member newMember) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Member oldmember = session.get(Member.class, newMember.getId());
+			final Boolean pass = newMember.getPass();
+			if (pass != null ) {
+				oldmember.setPass(pass);
+			}
+			final Integer roldId = newMember.getRoleId();
+			if (roldId != null ) {
+				oldmember.setRoleId(roldId);
+			}
+			transaction.commit();
+			return 1;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			
+		} 
+		return -1;
+	};
+	
+	
+	public Member selectById(Integer id) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Member member = session.get(Member.class, id);
+			transaction.commit();
+			return member;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return null;
+		} 
+		
+	};
+	
+	public List<Member> selectAll() {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+//			Query<Member> query = session.createQuery("FROM Member", Member.class);
+			Query<Member> query = session.createQuery(
+					"SELECT new web.member.pojo.Member(username, nickname) FROM Member", Member.class);
+			
+			List<Member> list = query.getResultList();
+			transaction.commit();
+			return list;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return null;
 		} 
 		
 	};
